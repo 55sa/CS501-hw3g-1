2c3ae942a5eff5ab74e185e269abd1890169e79b
charlesson7777 <123232898+charlesson7777@users.noreply.github.com>
2024-09-20 19:49:14 -0400

    Second update

--- app/src/main/java/com/example/hw3g_1/MainActivity.kt
+++ app/src/main/java/com/example/hw3g_1/MainActivity.kt
@@ -4,49 +4,33 @@ import android.os.Bundle
 import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hw3g_1.ui.theme.Hw3g1Theme
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextDecoration
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Hw3g1Theme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ){
-
+                    ListedTasks()
                 }
             }
         }
@@ -55,8 +39,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ListedTasks() {
-    var taskDescription by remember{ mutableStateOf("") }
-    var tasks by remember { mutableStateOf("") }
+    var taskDescription by remember{ mutableStateOf(TextFieldValue("")) }
+    var tasks by remember { mutableStateOf(mutableListOf<Task>())}
 
     Column(
         modifier = Modifier.fillMaxSize().padding(14.dp),
@@ -86,37 +70,53 @@ fun ListedTasks() {
             },
             modifier = Modifier
                 .padding(vertical = 7.dp)
-                .fillMaxSize()
+
         ){
             Text("Add Task")
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
-        TaskList(tasks = tasks, onTaskCheckedChange = {task, isChecked -> task.isCompleted = isChecked})
+        Spacer(modifier = Modifier.height(18.dp))
 
+        TaskList(tasks = tasks, onTaskCheckedChange = { task, isChecked -> task.isCompleted = isChecked})
 
+        Spacer(modifier = Modifier.height(18.dp))
 
-
+        Button(
+            onClick = {
+                tasks = tasks.filter { !it.isCompleted }.toMutableList()
+            },
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text("Clear Completed")
+        }
     }
 }
 
+data class Task(
+    val description: String,
+    var isCompleted: Boolean = false
+)
 @Composable
-fun TaskList(tasks: String, onTaskCheckedChange: Any) {
+fun TaskList(
+    tasks: List<Task>,
+    onTaskCheckedChange: (Task, Boolean) -> Unit
+){
+        Column(
+            modifier = Modifier.fillMaxWidth()) {
+            tasks.forEach { task ->
+                TaskRow(task = task, onCheckedChange = { isChecked ->
+                    onTaskCheckedChange(task, isChecked)
+                })
+            }
+        }
+    }
+
+
+@Composable
+fun TaskRow(task: Task,
+            onCheckedChange: (Boolean) -> Unit  ){
 
 }
-
-@Composable
-fun Task(){
-
-}
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
 
 @Preview(showBackground = true)
 @Composable
